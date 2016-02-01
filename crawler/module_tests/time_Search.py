33c7339eda99c24e2cff1565e64aad8529b5448d
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2016-02-01 23:19:16 +0100

    Changed: #1 TimeSearch Crawler for Reddit
    Finally we could gather all posts from the iAMA-Subreddits!
    I have to rewrite this routine to get it working correctly!
    more to come in the next days...

--- crawler/module_tests/time_Search.py
+++ crawler/module_tests/time_Search.py
@@ -1,22 +1,15 @@
 import praw
-
-
-
-
 reddit_Instance                     =               praw.Reddit(user_agent = "University_Regensburg_iAMA_Crawler_0.001")                # main reddit functionality
-reddit_Instance.get_redditor('C0rvuss')
 
-#reddit_Chosen_Subreddit             =               reddit_Instance.get_subreddit("iAma")                                               # the subreddit, which is to be crawled
-#reddit_Amount_Of_Threads_To_Crawl   =               2                                                                                 # the amount of threads during crawling
-# reddit_Metric_Of_Crawling           =               reddit_Chosen_Subreddit.get_hot(limit = reddit_Amount_Of_Threads_To_Crawl)          # the used metric of crawling.. See foled comment for options
 
-# submission_Thread = reddit_Instance.get_submission(submission_id='42zc3w')
 
-# timestamp not working here ... it is not UTC !!!
+# correct utc converter: http://www.esqsoft.com/javascript_examples/date-to-epoch.htm
+# https://www.reddit.com/r/redditdev/comments/2zdyy2/praw_continue_getting_posts_after_given_post_id/   <<<<<<<<<<<<<<<<< LOOK AT IT FOR SHIFTING WINDOW !!!! und das dann in ner Loop machen !!!
+posts = reddit_Instance.search('timestamp:1295029800..1295116200', \
+                      subreddit='iAMA', \
+                      sort="new", \
+                      limit=100, \
+                      syntax="cloudsearch")
 
-# Source: https://www.reddit.com/r/redditdev/comments/3lo4gn/praw_and_timestamp_searches/
-posts = reddit_Instance.search('',subreddit='iAMA',sort='new',limit=None,syntax='cloudsearch',params={'timestamp':'1296597186..1296769986'})
-
-i = 0
 for submission in posts:
 	print (submission.id)
