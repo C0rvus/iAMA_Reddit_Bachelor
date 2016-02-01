441c7e6c01f527095a2ee0350832f13723a3a8c4
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2016-02-01 22:59:29 +0100

    Changed: #1 Timestamp search without UTC
    Now we have a way more faster access to reddits submission by using cloudsearch! but the timestamp is not in utc it is in something else.. have to figure it out asap

--- crawler/module_tests/time_Search.py
+++ crawler/module_tests/time_Search.py
@@ -4,16 +4,19 @@ import praw
 
 
 reddit_Instance                     =               praw.Reddit(user_agent = "University_Regensburg_iAMA_Crawler_0.001")                # main reddit functionality
-user = reddit_Instance.get_redditor('C0rvuss')
+reddit_Instance.get_redditor('C0rvuss')
 
-reddit_Chosen_Subreddit             =               reddit_Instance.get_subreddit("iAma")                                               # the subreddit, which is to be crawled
-reddit_Amount_Of_Threads_To_Crawl   =               2                                                                                 # the amount of threads during crawling
+#reddit_Chosen_Subreddit             =               reddit_Instance.get_subreddit("iAma")                                               # the subreddit, which is to be crawled
+#reddit_Amount_Of_Threads_To_Crawl   =               2                                                                                 # the amount of threads during crawling
 # reddit_Metric_Of_Crawling           =               reddit_Chosen_Subreddit.get_hot(limit = reddit_Amount_Of_Threads_To_Crawl)          # the used metric of crawling.. See foled comment for options
 
 # submission_Thread = reddit_Instance.get_submission(submission_id='42zc3w')
 
+# timestamp not working here ... it is not UTC !!!
 
 # Source: https://www.reddit.com/r/redditdev/comments/3lo4gn/praw_and_timestamp_searches/
-posts = reddit_Instance.search('',subreddit='iAMA',sort='new',limit=None,syntax='cloudsearch',params={'timestamp':'1420027200..1420070400'})
+posts = reddit_Instance.search('',subreddit='iAMA',sort='new',limit=None,syntax='cloudsearch',params={'timestamp':'1296597186..1296769986'})
 
-print (posts)
+i = 0
+for submission in posts:
+	print (submission.id)
