8f07f682c6a6f7d1a96c93a649b950b0b5db8ce7
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2016-03-12 21:45:37 +0100

    Changed: #5, #12, #14 Analyzer for top 100 comments v3
    Made that script modular and added correct docstring comments..
    Now this script can be used in a universal way..

--- analyzer/question_Distribution/test/test - Kopie.py
+++ analyzer/question_Distribution/test/test - Kopie.py
@@ -5,125 +5,57 @@
 import collections                  # Necessary to sort collections alphabetically
 import matplotlib.pyplot as plt     # Necessary to plot graphs with the data calculated
 import datetime                     # Necessary to do time calculation
-import sys                          # Necessary to use script arguments
 from pymongo import MongoClient     # Necessary to make use of MongoDB
 
+# The mongo client, necessary to connect to mongoDB
+mongo_DB_Client_Instance = MongoClient('localhost', 27017)
 
-def initialize_mongo_db_parameters():
-    """Instantiates all necessary variables for the correct usage of the mongoDB-Client
+# The data base instance for the threads
+mongo_DB_Threads_Instance_2009 = mongo_DB_Client_Instance.iAMA_Reddit_Threads_2009
 
-    Args:
-        -
-    Returns:
-        -
-    """
+# Contains all collection names of the thread database
+mongo_DB_Thread_Collection_2009 = mongo_DB_Threads_Instance_2009.collection_names()
 
-    global mongo_DB_Client_Instance
-    global mongo_DB_Threads_Instance
-    global mongo_DB_Thread_Collection
-    global mongo_DB_Comments_Instance
+# The data base instance for the comments
+mongo_DB_Comments_Instance_2009 = mongo_DB_Client_Instance.iAMA_Reddit_Comments_2009
 
-    mongo_DB_Client_Instance = MongoClient('localhost', 27017)
-    mongo_DB_Threads_Instance = mongo_DB_Client_Instance['iAMA_Reddit_Threads_' + argument_year]
-    mongo_DB_Thread_Collection = mongo_DB_Threads_Instance.collection_names()
-    mongo_DB_Comments_Instance = mongo_DB_Client_Instance['iAMA_Reddit_Comments_' + argument_year]
+# Will contain all analyzed time information for threads & comments
+list_To_Be_Plotted = []
 
-
-def check_script_arguments():
-    """Checks if enough and correct arguments have been given to run this script adequate
-
-    1. It checks in the first instance if enough arguments have been given
-    2. Afterwards it fills 'argument_year' with the first argument (str) and 'argument_sorting' with a boolean value,
-        by previously parsing and checking that value.
-
-    Args:
-        -
-    Returns:
-        -
-    """
-
-    global argument_year, argument_sorting
-
-    # Whenever not enough arguments were given
-    if len(sys.argv) <= 2:
-        print("Not enough arguments were given...")
-        print("Terminating script now!")
-        sys.exit()
-
-    else:
-
-        # Simple bool checker
-        bool_checker = ['true', 'True', '1', 'yes', 'Yes', 'ja', 'Ja']
-
-        # Parses the first argument to the variable
-        argument_year = str(sys.argv[1])
-
-        # Whenever the second argument is in the bool_checker list
-        if sys.argv[2] in bool_checker:
-            argument_sorting = True
-        else:
-            argument_sorting = False
+# Calculates the time difference between to time stamps in seconds
 
 
 def calculate_time_difference(comment_time_stamp, answer_time_stamp_iama_host):
-    """Calculates the time difference in seconds between the a comment and its answer from the iama host
 
-    1. The time stamps will be converted from epoch
-        into float and afterwards into str again
-        (necessary for correct subtraction)
-    2. Then the time stamps will be subtracted from each other
-    3. The containing time difference will be converted into seconds (int)
-
-    Args:
-        comment_time_stamp (str): The time stamp of the comment
-        answer_time_stamp_iama_host (str): The time stamp of the iAMA hosts answer
-    Returns:
-        time_difference_in_seconds (int) : The time difference of the comment and its answer by the iAMA host in seconds
-    """
-    print(type(comment_time_stamp))
-
-    # Converts the time_Value into float, otherwise it could not be processed any further...
+    # Converts the time_Value into float, otherwise it could not be processed
+    # any further...
     comment_time_value = float(comment_time_stamp)
     comment_time_converted = datetime.datetime.fromtimestamp(
         comment_time_value).strftime('%d-%m-%Y %H:%M:%S')
     comment_time_converted_for_subtraction = datetime.datetime.strptime(
         comment_time_converted, '%d-%m-%Y %H:%M:%S')
 
-    # Converts the time_Value into float, otherwise it could not be processed any further...
+    # Converts the time_Value into float, otherwise it could not be processed
+    # any further...
     answer_time_iama_host = float(answer_time_stamp_iama_host)
     answer_time_iama_host_converted = datetime.datetime.fromtimestamp(
         answer_time_iama_host).strftime('%d-%m-%Y %H:%M:%S')
     answer_time_iama_host_converted_for_subtraction = datetime.datetime.strptime(
         answer_time_iama_host_converted, '%d-%m-%Y %H:%M:%S')
 
-    # Calculates the time difference between the comment and the iAMA hosts answer
+    # Calculates the time difference between the comment and the iAMA hosts
+    # answer
     time_difference_in_seconds = (
         answer_time_iama_host_converted_for_subtraction -
-        comment_time_converted_for_subtraction
-    ).total_seconds()
+        comment_time_converted_for_subtraction).total_seconds()
 
     return time_difference_in_seconds
 
+# Checks whether the thread host has answered a given question
+
 
 def check_if_comment_is_answer_from_thread_author(author_of_thread, comment_acutal_id, comments_cursor):
-    """Checks whether both strings are equal or not
 
-    1. A dictionary containing flags whether that a question is answered by the host with the appropriate timestamp will
-        be created in the beginning.
-    2. Then the method iterates over every comment within that thread
-        1.1. Whenever an answer is from the iAMA hosts and the processed comments 'parent_id' matches the iAMA hosts
-            comments (answers) id, the returned dict will contain appropriate values and will be returned
-        1.2. If this is not the case, it will be returned in its default condition
-
-    Args:
-        author_of_thread (str) : The name of the thread author (iAMA-Host)
-        comment_acutal_id (str) : The id of the actually processed comment
-        comments_cursor (Cursor) : The cursor which shows to the amount of comments which can be iterated
-    Returns:
-        True (bool): Whenever the strings do not match
-        False (bool): Whenever the strings do match
-         answered that given question)
-    """
     dict_to_be_returned = {
         "question_Answered_From_Host": False,
         "time_Stamp_Answer": 0
@@ -140,7 +72,8 @@ def check_if_comment_is_answer_from_thread_author(author_of_thread, comment_acut
 
             # Whenever the iterated comment is from the iAMA-Host and that
             # comment has the question as parent_id
-            if (check_if_comment_is_not_from_thread_author(
+            if (
+                    check_if_comment_is_not_from_thread_author(
                         author_of_thread,
                         actual_comment_author) == False) and (
                     check_comment_parent_id == comment_acutal_id):
@@ -158,76 +91,35 @@ def check_if_comment_is_answer_from_thread_author(author_of_thread, comment_acut
     # This is the case whenever a comment has not a single thread
     return dict_to_be_returned
 
+# Checks whether the postet comment is not from the thread creator
+
 
 def check_if_comment_is_not_from_thread_author(author_of_thread, comment_author):
-    """Checks whether both strings are equal or not
 
-    1. This method simply checks wether both strings match each other or not.
-        I have built this extra method to have a better overview in the main code..
-
-    Args:
-        author_of_thread (str) : The name of the thread author (iAMA-Host)
-        comment_author (str) : The name of the comments author
-    Returns:
-        True (bool): Whenever the strings do not match
-        False (bool): Whenever the strings do match
-         answered that given question)
-    """
     if author_of_thread != comment_author:
         return True
     else:
         return False
 
+# Could be expanded later on, if checking for question mark is not enough
+
 
 def check_if_comment_is_a_question(given_string):
-    """Simply checks whether a given string is a question or not
-
-    1. This method simply checks wether a question mark exists within that string or not..
-        This is just that simple because messing around with natural processing kits to determine the semantic sense
-        would blow up my bachelor work...
-
-    Args:
-        given_string (int) : The string which will be checked for a question mark
-    Returns:
-        True (bool): Whenever the given string is a question
-        False (bool): Whenever the given string is not a question
-
-    """
 
     if "?" in given_string:
         return True
     else:
         return False
 
+# Calculates some values for later correlation checking between thread creation date, upvote and answer possibility
+
 
 def calculate_answered_question_upvote_correlation(id_of_thread, author_of_thread, thread_creation_date):
-    """ Checks whether an iterated question has been answered by the iama host or not
-
-    1. This method checks at first whether an iterated comment contains values (e.g. is not none)
-        1.1. If not: That comment will be skipped / if no comment is remaining None will be returned
-        1.2. If yes: That comment will be processed
-    2. Now it will be checked whether that iterated comment is a question or not
-    3. Afterwards it will be checked wether that comment is a comment from the iAMA Host or not
-        3.1. If this is not the case the next comment will be processed
-    4. Whenever that processed comment is a question and not (!!) from the thread author:
-        amount_of_tier_any_questions (int) will be increased by one
-    5. Now it will be checked whether that comment has a comment ( answer ) below it which is from the iAMA-host
-        5.1. If yes: amount_of_tier_any_questions_answered (int) will be increased by one and the dictionary, which
-            is to be returned will be filled with values
-        5.2. If no: the dictionary, which is to be returned will be filled with values
-
-    Args:
-        id_of_thread (str) : Contains the id of the thread which is to be iterated
-        author_of_thread (str) : Contains the name of the thread author
-        thread_creation_date (int): Contains the time
-    Returns:
-        amount_of_questions_not_answered (int) : The amount of questions which have not been answered
-    """
 
     # Makes the global comments instance locally available here
-    global mongo_DB_Comments_Instance
+    global mongo_DB_Comments_Instance_2009
 
-    comments_collection = mongo_DB_Comments_Instance[id_of_thread]
+    comments_collection = mongo_DB_Comments_Instance_2009[id_of_thread]
     comments_cursor = comments_collection.find()
 
     amount_of_results = []
@@ -317,32 +209,17 @@ def calculate_answered_question_upvote_correlation(id_of_thread, author_of_threa
         return None
 
 
+# Generates the data which will be analyzed later on
+
+
 def generate_data_to_analyze():
-    """Generates the data which will be analyzed
+    for j, val in enumerate(mongo_DB_Thread_Collection_2009):
 
-    1. This method iterates over every thread
-        1.1. It filters if that iterated thread is an iAMA-request or not
-            1.1.1. If yes: this thread gets skipped and the next one will be processed
-            1.1.2. If no: this thread will be processed
-    2. If the thread gets processed it will receive an ordered dictionary containing information about every question
-        whether it has been answered or not
-    3. This ordered dictionary will be applied to a global list, which will be processed after wards for the generation
-        of plots
-
-    Args:
-        -
-    Returns:
-        -
-    """
-
-    print("Generating data now...")
-    # noinspection PyTypeChecker
-    for j, val in enumerate(mongo_DB_Thread_Collection):
         # Skips the system.indexes-table which is automatically created by
         # mongodb itself
         if not val == "system.indexes":
             # References the actual iterated thread
-            temp_thread = mongo_DB_Threads_Instance[val]
+            temp_thread = mongo_DB_Threads_Instance_2009[val]
 
             # Gets the authors name of the iterated thread
             temp_thread_author = temp_thread.find()[0].get("author")
@@ -363,25 +240,17 @@ def generate_data_to_analyze():
 
             returned_value = calculate_answered_question_upvote_correlation(
                 val, temp_thread_author, temp_thread_creation_date
+
             )
 
             # Value could be none if it has i.E. no values
             if returned_value is not None:
                 list_To_Be_Plotted.append(returned_value)
 
+# Prepares the data which will be plotted later. (Does some sorting)
+
 
 def prepare_and_print_data_to_be_plotted():
-    """Prepares data and prints data into the command line
-
-    1. This method prepares the data, in kind of sorting and counting amount of questions not being answered
-    2. Afterwards it prints, in dependency of the second argument given of this script, whether the
-        TOP or WORST 100 questions have been answered or not
-
-    Args:
-        -
-    Returns:
-        amount_of_questions_not_answered (int) : The amount of questions which have not been answered
-    """
 
     # Will contain all comments later on
     all_comments = []
@@ -399,8 +268,7 @@ def prepare_and_print_data_to_be_plotted():
             all_comments.append(val_2)
 
     # Creates a "sorted" which contains all comments of that year, sorted by upvotes in descending order
-    # In dependence of given sort parameter..
-    new_list = sorted(all_comments, key=lambda k: k['question_ups'], reverse=argument_sorting)
+    new_list = sorted(all_comments, key=lambda k: k['question_ups'], reverse=True)
 
     print("---- Printing top 100 comments now")
 
@@ -434,17 +302,6 @@ def prepare_and_print_data_to_be_plotted():
 
 
 def plot_generated_data(amount_of_questions_not_answered):
-    """Plots the data which is to be generated
-
-    This method plots the data which has been calculated before by using
-    matplotlib.pyplot-library
-
-    Args:
-        amount_of_questions_not_answered (int): The amount of questions which have not been answered.
-    Returns:
-        -
-    """
-
     plt.figure()
     labels = ['Nicht beantwortet', 'Beantwortet']
     colors = ['yellowgreen', 'gold']
@@ -454,50 +311,13 @@ def plot_generated_data(amount_of_questions_not_answered):
     plt.pie(values, colors=colors, autopct='%.2f%%')
 
     plt.legend(patches, labels, loc="upper right")
-
-    if argument_sorting is True:
-        plt.title('iAMA ' + argument_year + ' - Beantwortung der TOP 100 Fragen')
-    else:
-        plt.title('iAMA ' + argument_year + ' - Beantwortung der WORST 100 Fragen')
+    plt.title('iAMA 2009 - Beantwortung der TOP 100 Fragen')
 
     # Set aspect ratio to be equal so that pie is drawn as a circle.
     plt.axis('equal')
     plt.tight_layout()
     plt.show()
 
-
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Necessary variables and scripts are here
-
-# Contains the year which is given as an argument
-argument_year = ""
-
-# Contains the information wether the first top 100 (highest upvotes) or the last 100 (lowest upvotes [negative]
-# should be calculated
-argument_sorting = bool
-
-# The mongo client, necessary to connect to mongoDB
-mongo_DB_Client_Instance = None
-
-# The data base instance for the threads
-mongo_DB_Threads_Instance = None
-
-# Contains all collection names of the thread database
-mongo_DB_Thread_Collection = None
-
-# The data base instance for the comments
-mongo_DB_Comments_Instance = None
-
-# Will contain all analyzed time information for threads & comments
-list_To_Be_Plotted = []
-
-
-# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Methods which are to be called are here
-
-# Executes necessary checks
-check_script_arguments()
-
-# Initializes the mongoDB with the arguments given via command line
-initialize_mongo_db_parameters()
 
 # Generates the data which will be plotted later on
 generate_data_to_analyze()
