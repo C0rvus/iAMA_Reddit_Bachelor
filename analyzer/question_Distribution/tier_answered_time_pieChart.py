e8aafacb8daff72458611bbc68d233357913f31a
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2016-03-13 12:35:28 +0100

    Changed: #14, #13, #12, #7 for time-tier-analyzer

--- analyzer/question_Distribution/tier_answered_time_pieChart.py
+++ analyzer/question_Distribution/tier_answered_time_pieChart.py
@@ -4,9 +4,6 @@ import sys                          # Necessary to use script arguments
 from pymongo import MongoClient     # Necessary to make use of MongoDB
 import numpy as np                  # Necessary for mean calculation
 
-# TODO: Für alle Tiers machen.. Tier 1, X, Any
-# TODO: Mit Unterscheidung zu Stunden / Minuten
-
 
 def initialize_mongo_db_parameters():
     """Instantiates all necessary variables for the correct usage of the mongoDB-Client
@@ -40,7 +37,7 @@ def check_script_arguments():
         -
     """
 
-    global argument_year, argument_tier_to_be_in_scope, argument_plot_time_unit
+    global argument_year, argument_tier_in_scope, argument_plot_time_unit
 
     # Whenever not enough arguments were given
     if len(sys.argv) <= 3:
@@ -50,7 +47,7 @@ def check_script_arguments():
     else:
         # Parses the first argument to the variable
         argument_year = str(sys.argv[1])
-        argument_tier_to_be_in_scope = str(sys.argv[2]).lower()
+        argument_tier_in_scope = str(sys.argv[2]).lower()
         argument_plot_time_unit = str(sys.argv[3]).lower()
 
 
@@ -204,11 +201,24 @@ def check_if_comment_is_a_question(given_string):
         return False
 
 
-
-
-
-# TODO: Duplicate code machen für die Tiers here ?
 def calculate_ar_mean_answer_time_for_questions(id_of_thread, author_of_thread):
+    """Calculates the arithmetic mean of the answer time by the iama host in minutes
+
+    In dependence of the given tier argument (second argument) the processing of tiers will be filtered
+
+    Args:
+        id_of_thread (str): The id of the thread which is actually processed. (Necessary for checking if a question
+            lies on tier 1 or any other tier)
+        author_of_thread (str): The name of the thread author. (Necessary for checking if a given answer is from the
+            iama host or not)
+    Returns:
+        Whenever there was a minimum of 1 question asked and 1 answer from the iama host:
+            amount_of_answer_times (int) : The amount of the arithmetic mean time of
+        Whenever there no questions have been asked for that thread / or no answers were given /
+            or all values in the database were null:
+            None:   Returns an empty object of the type None
+    """
+
     global mongo_DB_Comments_Instance
 
     comments_collection = mongo_DB_Comments_Instance[id_of_thread]
@@ -216,8 +226,8 @@ def calculate_ar_mean_answer_time_for_questions(id_of_thread, author_of_thread):
 
     amount_of_answer_times = []
 
-    amount_of_tier_x_questions = 0
-    amount_of_tier_x_questions_answered = 0
+    amount_of_questions = 0
+    amount_of_questions_answered = 0
 
     # Iterates over every comment within that thread
     for collection in comments_cursor:
@@ -237,66 +247,136 @@ def calculate_ar_mean_answer_time_for_questions(id_of_thread, author_of_thread):
                     and comment_author is not None \
                     and comment_parent_id is not None:
 
-                bool_comment_is_question = check_if_comment_is_a_question(
-                    comment_text)
-                bool_comment_is_question_on_tier_1 = check_if_comment_is_on_tier_1(
-                    comment_parent_id)
+                bool_comment_is_question = check_if_comment_is_a_question(comment_text)
+
+                bool_comment_is_question_on_tier_1 = check_if_comment_is_on_tier_1(comment_parent_id)
+
                 bool_comment_is_not_from_thread_author = check_if_comment_is_not_from_thread_author(
                     author_of_thread, comment_author)
 
-                # If the posted comment is a question and is not from the
-                # thread author and is on Tier 1
-                if bool_comment_is_question \
-                        and bool_comment_is_question_on_tier_1 is False \
-                        and bool_comment_is_not_from_thread_author is True:
+                # Whenever the scope lies on the first tier
+                if argument_tier_in_scope == "1":
 
-                    amount_of_tier_x_questions += 1
+                    if bool_comment_is_question \
+                            and bool_comment_is_question_on_tier_1 \
+                            and bool_comment_is_not_from_thread_author:
 
-                    # Check whether that iterated comment is answered by the
-                    # host
-                    answer_is_from_thread_author = check_if_comment_is_answer_from_thread_author(
-                        author_of_thread, comment_acutal_id, comments_cursor)
+                        amount_of_questions += 1
 
-                    # Whenever the answer to that comment is from the author
-                    # (boolean == True)
-                    if answer_is_from_thread_author[
-                            "question_Answered_From_Host"] is True:
-                        answer_time_stamp_iama_host = answer_is_from_thread_author[
-                            "time_Stamp_Answer"]
+                        # Check whether that iterated comment is answered by the host
+                        answer_is_from_thread_author = check_if_comment_is_answer_from_thread_author(
+                            author_of_thread, comment_acutal_id, comments_cursor)
 
-                        # Adds the calculated answer time to a local list
-                        amount_of_answer_times.append(
-                            calculate_time_difference(
-                                comment_time_stamp,
-                                answer_time_stamp_iama_host))
+                        # Whenever the answer to that comment is from the author
+                        # (boolean == True)
+                        if answer_is_from_thread_author["question_Answered_From_Host"] is True:
 
-                        amount_of_tier_x_questions_answered += 1
+                            answer_time_stamp_iama_host = answer_is_from_thread_author["time_Stamp_Answer"]
 
-                # Skip that comment
+                            # Adds the calculated answer time to a local list
+                            amount_of_answer_times.append(
+                                calculate_time_difference(
+                                    comment_time_stamp,
+                                    answer_time_stamp_iama_host)
+                            )
+
+                            amount_of_questions_answered += 1
+
+                    # Skip that comment
+                    else:
+                        continue
+
+                # Whenever the scope lies on any other tier except tier 1
+                elif argument_tier_in_scope == "x":
+
+                    # If the posted comment is a question and is not from the thread author and is not on Tier 1
+                    if bool_comment_is_question \
+                            and bool_comment_is_question_on_tier_1 is False \
+                            and bool_comment_is_not_from_thread_author is True:
+
+                        amount_of_questions += 1
+
+                        # Check whether that iterated comment is answered by the host
+                        answer_is_from_thread_author = check_if_comment_is_answer_from_thread_author(
+                            author_of_thread, comment_acutal_id, comments_cursor)
+
+                        # Whenever the answer to that comment is from the author
+                        if answer_is_from_thread_author["question_Answered_From_Host"] is True:
+                            answer_time_stamp_iama_host = answer_is_from_thread_author["time_Stamp_Answer"]
+
+                            # Adds the calculated answer time to a local list
+                            amount_of_answer_times.append(
+                                calculate_time_difference(
+                                    comment_time_stamp,
+                                    answer_time_stamp_iama_host)
+                            )
+
+                            amount_of_questions_answered += 1
+
+                    # Skip that comment
+                    else:
+                        continue
+
+                # Whenever the scope lies on all tiers
                 else:
-                    continue
 
-            # Whenever a comment has been deleted or has, somehow, null values
-            # in it.. do not process it
+                    if bool_comment_is_question and bool_comment_is_not_from_thread_author is True:
+
+                        amount_of_questions += 1
+
+                        # Check whether that iterated comment is answered by the host
+                        answer_is_from_thread_author = check_if_comment_is_answer_from_thread_author(
+                            author_of_thread, comment_acutal_id, comments_cursor)
+
+                        # Whenever the answer to that comment is from the author
+                        if answer_is_from_thread_author["question_Answered_From_Host"] is True:
+                            answer_time_stamp_iama_host = answer_is_from_thread_author["time_Stamp_Answer"]
+
+                            # Adds the calculated answer time to a local list
+                            amount_of_answer_times.append(
+                                calculate_time_difference(
+                                    comment_time_stamp,
+                                    answer_time_stamp_iama_host)
+                            )
+
+                            amount_of_questions_answered += 1
+
+                    # Skip that comment
+                    else:
+                        continue
+
+            # Whenever a comment has been deleted or has, somehow, null values in it.. do not process it
             else:
                 continue
 
-    # Whenever there were some questions aksed on tier X and those questions
-    # have been answered by the iAMA host on tier X
-    if amount_of_tier_x_questions != 0 and amount_of_tier_x_questions_answered != 0:
+    # Whenever some questions have been asked and they have received an answer
+    if amount_of_questions != 0 and amount_of_questions_answered != 0:
 
         # Returns the arithmetic mean of answer time by the iAMA host
         return np.mean(amount_of_answer_times)
 
-    # Whenever there were no tier X questions asked.. so all questions remained on tier 1
+    # Whenever no questions have been asked at all !
     else:
         return None
 
 
-# Generates the data which will be analyzed later on
 def generate_data_to_analyze():
+    """Generates the data which will be analyzed
+
+    1. This method iterates over every thread
+        1.1. It filters if that iterated thread is an iAMA-request or not
+            1.1.1. If yes: this thread gets skipped and the next one will be processed
+            1.1.2. If no: this thread will be processed
+    2. If the thread gets processed it will receive the arithmetic mean of answer time
+    3. This value will be added to a global list and will be plotted later on
+    Args:
+        -
+    Returns:
+        -
+    """
 
     print("Generating data now...")
+
     # noinspection PyTypeChecker
     for j, val in enumerate(mongo_DB_Thread_Collection):
 
@@ -320,80 +400,150 @@ def generate_data_to_analyze():
                     and "request response" not in temp_thread_title.lower():
                 continue
 
-            returned_value = calculate_ar_mean_answer_time_for_questions(
-                val, temp_thread_author)
+            returned_value = calculate_ar_mean_answer_time_for_questions(val, temp_thread_author)
 
             # Value could be none if it has i.E. no values
             if returned_value is not None:
                 list_To_Be_Plotted.append(returned_value)
 
-# Plots the data of the question distribution for that year
-
 
 def plot_the_generated_data_percentage_mean():
+    """Plots the data which is to be generated
 
-    # The dictionary which is necessary to count the amount of response times
-    # in Minutes
-    dict_time_amount_counter = {
-        "0_To_1": 0,
-        "1_To_6": 0,
-        "6_To_12": 0,
-        "12_To_24": 0,
-        "24_To_48": 0,
-        "greater_Than_48": 0
-    }
+    1. This method plots the data which has been calculated before by using 'matplotlib.pyplot-library'
+    2. In dependence of the chosen time unit the values will be seperated in either minutes or hours
 
-    # Iterates over every value and fills the dict_time_amount_counter
-    # appropriate
-    for i, val in enumerate(list_To_Be_Plotted):
+    Args:
+        -
+    Returns:
+        -
+    """
 
-        if 0 < (val / 3600) <= 1:
-            dict_time_amount_counter["0_To_1"] += 1
-
-        elif 1 < (val / 3600) <= 6:
-            dict_time_amount_counter["1_To_6"] += 1
-
-        elif 6 < (val / 3600) <= 12:
-            dict_time_amount_counter["6_To_12"] += 1
-
-        elif 12 < (val / 3600) <= 24:
-            dict_time_amount_counter["12_To_24"] += 1
-
-        elif 24 < (val / 3600) <= 48:
-            dict_time_amount_counter["24_To_48"] += 1
-
-        elif (val / 3600) > 48:
-            dict_time_amount_counter["greater_Than_48"] += 1
-
-    plt.figure()
-
-    # The slices will be ordered and plotted counter-clockwise.
-    labels = [
-        '0 bis 1 h',
-        '1 bis 6 h',
-        '6 bis 12 h',
-        '12 bis 24 h',
-        '24 bis 48 h',
-        '> 48 h']
     colors = [
         'yellowgreen',
         'gold',
         'lightskyblue',
         'lightcoral',
         'mediumpurple',
-        'orange']
-    values = [dict_time_amount_counter['0_To_1'],
-              dict_time_amount_counter['1_To_6'],
-              dict_time_amount_counter['6_To_12'],
-              dict_time_amount_counter['12_To_24'],
-              dict_time_amount_counter['24_To_48'],
-              dict_time_amount_counter['greater_Than_48']]
+        'orange'
+    ]
 
-    patches, texts = plt.pie(values, colors=colors, startangle=90, shadow=True)
-    plt.pie(values, colors=colors, autopct='%.2f%%')
+    # Whenever the given time argument is minutes..
+    if argument_plot_time_unit == "min":
 
-    plt.legend(patches, labels, loc="upper left")
-    plt.title('iAMA ' + argument_year + ' - Ø Reaktionszeit des iAMA-Host auf Fragen auf Ebene X in Stunden')
+        # The dictionary which is necessary to count the amount of response times in minutes
+        dict_time_amount_counter = {
+            "0_To_5": 0,
+            "5_To_15": 0,
+            "15_To_30": 0,
+            "30_To_60": 0,
+            "60_To_120": 0,
+            "greater_Than_120": 0,
+        }
+
+        for i, val in enumerate(list_To_Be_Plotted):
+            if 0 < (val / 60) <= 5:
+                dict_time_amount_counter["0_To_5"] += 1
+
+            elif 5 < (val / 60) <= 15:
+                dict_time_amount_counter["5_To_15"] += 1
+
+            elif 15 < (val / 60) <= 30:
+                dict_time_amount_counter["15_To_30"] += 1
+
+            elif 30 < (val / 60) <= 60:
+                dict_time_amount_counter["30_To_60"] += 1
+
+            elif 60 < (val / 60) <= 120:
+                dict_time_amount_counter["60_To_120"] += 1
+
+            elif (val / 60) > 120:
+                dict_time_amount_counter["greater_Than_120"] += 1
+
+        # The slices will be ordered and plotted counter-clockwise.
+        labels = [
+            '0 bis 5 min',
+            '5 bis 15 min',
+            '15 bis 30 min',
+            '30 bis 60 min',
+            '60 bis 120 min',
+            '> 120 min'
+        ]
+
+        values = [
+            dict_time_amount_counter['0_To_5'],
+            dict_time_amount_counter['5_To_15'],
+            dict_time_amount_counter['15_To_30'],
+            dict_time_amount_counter['30_To_60'],
+            dict_time_amount_counter['60_To_120'],
+            dict_time_amount_counter['greater_Than_120']
+        ]
+
+        plt.figure()
+        patches, texts = plt.pie(values, colors=colors, startangle=90, shadow=True)
+        plt.pie(values, colors=colors, autopct='%.2f%%')
+        plt.legend(patches, labels, loc="lower right", bbox_to_anchor=(1.2, 0.25))
+        plt.title('iAMA ' + argument_year + ' - Ø Reaktionszeit des iAMA-Host auf Fragen auf Ebene ' +
+                  argument_tier_in_scope +
+                  ' in Minuten')
+
+    else:
+        # The dictionary which is necessary to count the amount of response times in hours
+        dict_time_amount_counter = {
+            "0_To_1": 0,
+            "1_To_6": 0,
+            "6_To_12": 0,
+            "12_To_24": 0,
+            "24_To_48": 0,
+            "greater_Than_48": 0
+        }
+
+        for i, val in enumerate(list_To_Be_Plotted):
+
+            if 0 < (val / 3600) <= 1:
+                dict_time_amount_counter["0_To_1"] += 1
+
+            elif 1 < (val / 3600) <= 6:
+                dict_time_amount_counter["1_To_6"] += 1
+
+            elif 6 < (val / 3600) <= 12:
+                dict_time_amount_counter["6_To_12"] += 1
+
+            elif 12 < (val / 3600) <= 24:
+                dict_time_amount_counter["12_To_24"] += 1
+
+            elif 24 < (val / 3600) <= 48:
+                dict_time_amount_counter["24_To_48"] += 1
+
+            elif (val / 3600) > 48:
+                dict_time_amount_counter["greater_Than_48"] += 1
+
+        # The slices will be ordered and plotted counter-clockwise.
+        labels = [
+            '0 bis 1 h',
+            '1 bis 6 h',
+            '6 bis 12 h',
+            '12 bis 24 h',
+            '24 bis 48 h',
+            '> 48 h'
+        ]
+
+        values = [
+            dict_time_amount_counter['0_To_1'],
+            dict_time_amount_counter['1_To_6'],
+            dict_time_amount_counter['6_To_12'],
+            dict_time_amount_counter['12_To_24'],
+            dict_time_amount_counter['24_To_48'],
+            dict_time_amount_counter['greater_Than_48']
+        ]
+
+        plt.figure()
+        patches, texts = plt.pie(values, colors=colors, startangle=90, shadow=True)
+        plt.pie(values, colors=colors, autopct='%.2f%%')
+        plt.legend(patches, labels, loc="upper left")
+        plt.title('iAMA ' + argument_year + ' - Ø Reaktionszeit des iAMA-Host auf Fragen auf Ebene ' +
+                  argument_tier_in_scope +
+                  ' in Stunden')
 
     # Set aspect ratio to be equal so that pie is drawn as a circle.
     plt.axis('equal')
@@ -407,7 +557,7 @@ def plot_the_generated_data_percentage_mean():
 argument_year = ""
 
 # Contains the tiers which will be in scope for the calculation
-argument_tier_to_be_in_scope = ""
+argument_tier_in_scope = ""
 
 # Contains the time unit in which the graphs will be plotted later on
 argument_plot_time_unit = ""
