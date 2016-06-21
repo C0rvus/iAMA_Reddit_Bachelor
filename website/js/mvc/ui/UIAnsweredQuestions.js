71d8e493007131ec309570f4ce35679fc2d4ee57
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2016-06-21 12:31:45 +0200

    Changed: #36 Correct display of q&a of answered questions
    Now the questions and the appropriate answers will be displayed correctly!

--- website/js/mvc/ui/UIAnsweredQuestions.js
+++ website/js/mvc/ui/UIAnsweredQuestions.js
@@ -1,4 +1,5 @@
 // Source: http://stackoverflow.com/questions/759887/how-to-create-a-dom-node-as-an-object
+// http://stackoverflow.com/questions/5076466/javascript-replace-n-with-br
 
 IAMA_Extension.UIAnsweredQuestions = function () {
     var that = {},
@@ -15,62 +16,83 @@ IAMA_Extension.UIAnsweredQuestions = function () {
             // Removes the first example answer here
             $( "#iAMA_Answer_Panel").find("> li" ).remove();
 
+            // Iterates over every q&a combination
             $.each( data, function( key, value ) {
 
                 var answer_id = value['answer_id'],
-                    answer_text = value['answer_text'],
+                    answer_text = value['answer_text'].replace(new RegExp('\r?\n','g'), '<br>'),
                     answer_timestamp = value['answer_timestamp'],
                     answer_upvote_score = value['answer_upvote_score'],
 
                     question_author = value['question_author'],
                     question_id = value['question_id'],
-                    question_text = value['question_text'],
+                    question_text = value['question_text'].replace(new RegExp('\r?\n','g'), '<br>'),
                     question_timestamp = value['question_timestamp'],
-                    question_upvote_score = value['question_upvote_score'];
+                    question_upvote_score = value['question_upvote_score'],
 
-                // TODO : That one here is necessary for different class behaviour
+                    li_top = null;
+
+                // Necessary for different messaging color
                 if (key % 2 === 0 || key === 0) {
-                    console.log(key, "Ich bin gerade !! also gleich 0")
+                    li_top = $("<li class='left clearfix chat-message-0' id='" + answer_id + "'" + "> </li>");
+
+                } else {
+                    li_top = $("<li class='left clearfix chat-message-1' id='" + answer_id + "'" + "> </li>");
                 }
 
+                // Defining DOM Elements here
+                var separator_q_n_a = $("<hr class='ruler-answered-questions'>"),
+
+                    // Building question containers here
+                    q_uber_container = $("<div class='left clearfix'></div>"),
+                    q_header = $("<div class='header'></div>"),
+
+                    // Question elements are defined here
+                    q_p_username = $("<p class='pull-left primary-font strong'>" + question_author + "</p>"),
+                    q_p_posting_time = $("<p class='pull-right text-muted small chat-answered-questions'>" + question_timestamp + "<i class='fa fa-clock-o fa-fw'></i> </p> <br>"),
+                    q_p_score = $("<p class='pull-right text-muted small chat-answered-questions'>" + question_upvote_score + "<i class='fa fa-bullseye fa-fw'> </i> </p> <br>"),
+                    q_p_question_id = $("<p class='pull-right text-muted small chat-answered-questions'>" + question_id + "<i class='fa fa-bookmark fa-fw'> </i> </p> <br> <br>"),
+                    q_p_text = $("<p class='chat-alignment-left'>" + question_text + "</p>"),
+
+                    a_uber_container = $("<div class='right clearfix'></div>"),
+                    a_header = $("<div class='header'></div>"),
+                    a_p_username = $("<p class='pull-right primary-font strong'>" + "You" + "</p><br>"),
+                    a_p_posting_time = $("<p class='pull-left text-muted small chat-answered-questions'><i class='fa fa-clock-o fa-fw'></i>" + answer_timestamp + "</p><br>"),
+                    a_p_score = $("<p class='pull-left text-muted small chat-answered-questions'><i class='fa fa-bullseye fa-fw'></i>" + answer_upvote_score + "</p><br>"),
+                    a_p_answer_id = $("<p class='pull-left text-muted small chat-answered-questions'><i class='fa fa-bookmark fa-fw'></i>" + answer_id + "</p><br><br>"),
+                    a_p_text = $("<p class='chat-alignment-right'>" + answer_text + "</p>");
+
+                // Chains the question objects all together
+                q_header.appendTo(q_uber_container);
+                q_p_username.appendTo(q_header);
+                q_p_posting_time.appendTo(q_header);
+                q_p_score.appendTo(q_header);
+                q_p_question_id.appendTo(q_header);
+                q_p_text.appendTo(q_header);
+
+                // Chains the answer objects all together
+                a_header.appendTo(a_uber_container);
+                a_p_username.appendTo(a_header);
+                a_p_posting_time.appendTo(a_header);
+                a_p_score.appendTo(a_header);
+                a_p_answer_id.appendTo(a_header);
+                a_p_text.appendTo(a_header);
+
+                // Appends all container objects to the top level DOM <li> element
+                q_uber_container.appendTo(li_top);
+                separator_q_n_a.appendTo(li_top);
+                a_uber_container.appendTo(li_top);
+
+                // Adds that combination to the answer panel
+                answer_Panel.append(li_top);
 
 
-                // Hier in jedem Element nochmal drueber gehen
+                //TODO: Timestamp conversion (subtraction to today)
+
             });
 
 
 
-            // TODO: Hier schauen mittels iteration und modulo 2 ob gerade Zahl oder nicht, und das reinbauen
-            var li_top = $("<li class='left clearfix chat-message-0'> </li>");
-
-            var separator_q_n_a = $("<hr class='ruler-answered-questions'>");
-
-            var combined = $(li_top).append(separator_q_n_a);
-
-
-            // Building question containers here
-            var q_uber_container = $("<div class='left clearfix'> </div>");
-            var q_header = $("<div class='header'> </div>");
-
-            var q_p_username = $("<p class='pull-left primary-font strong'> </p>"); //TODO: Username in here
-
-            var q_p_posting_time = $("<p class='pull-right text-muted small chat-answered-questions'> </p>"); // TODO: Posting time
-            var q_i_posting_time_icon = $("<i class='fa fa-clock-o fa-fw'> </i>");
-
-            var break_line = $("<br>");
-
-            var q_p_score = $("<p class='pull-right text-muted small chat-answered-questions'> </p>"); // TODO: Amount of points
-            var q_i_score = $("<i class='fa fa-bullseye fa-fw'> </i>");
-
-            var q_p_question_id = $("<p class='pull-right text-muted small chat-answered-questions'> </p>"); // TODO: Amount of points
-            var q_i_question_id = $("<i class='fa fa-bullseye fa-fw'> </i>");
-
-            var q_p_text = $("<p class='chat-alignment-left'> </p>"); //TODO Question text in here
-
-
-            answer_Panel.append(combined);
-
-            // TODO: Das hier testweise mal zusammenbauen!
 
         },
 
