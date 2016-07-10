98f2df6bb57f11d7f134371768750d63eb165400
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2016-07-10 17:21:48 +0200

    Changed: #60, #39, #34, #18, #64 Reworked / created documentation for all scripts ever created

---
+++ python_n_webpage/_documentation_Code/Webpage/create_Documentation.bat
@@ -0,0 +1,3 @@
+@Echo off
+
+jsdoc IAMA_Extension.js MainController.js ".\rest\RestController.js" ".\rest\MongoDBConnector.js" ".\ui\UIController.js" ".\ui\UIAnsweredQuestions.js" ".\ui\UIGenericMethods.js" ".\ui\UIStatsOverview.js" ".\ui\UIThreadOverview.js" ".\ui\UITopBar.js" ".\ui\UIUnansweredQuestions.js" 
