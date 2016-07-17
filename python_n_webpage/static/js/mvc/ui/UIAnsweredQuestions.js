4be1c64bba5a6bd598c671b5228000c540fa4d2a
Benedikt Hierl <Benedikt.Hierl@gmx.de>
2016-07-17 08:30:22 +0200

    Changed: #70 UI Rework v4

--- python_n_webpage/static/js/mvc/ui/UIAnsweredQuestions.js
+++ python_n_webpage/static/js/mvc/ui/UIAnsweredQuestions.js
@@ -410,7 +410,8 @@ IAMA_Extension.UIAnsweredQuestions = function () {
         _onAnswersToDOM = function (event, data) {
 
             // Removes the first example answer here
-            $( "#iAMA_Answer_Panel").find("> li" ).remove();
+            $("#iAMA_Answer_Panel").find("> li" ).remove();
+            $("#iAMA_Answer_Panel").find("> hr" ).remove();
 
             // Iterates over every q&a combination and patches the DOM elements together
             $.each( data, function( key, value ) {
@@ -424,9 +425,7 @@ IAMA_Extension.UIAnsweredQuestions = function () {
                     question_id = value['question_id'],
                     question_text = value['question_text'].replace(new RegExp('\r?\n','g'), '<br>'),
                     question_timestamp = value['question_timestamp'],
-                    question_upvote_score = value['question_upvote_score'],
-
-                    li_top = null;
+                    question_upvote_score = value['question_upvote_score'];
 
                 // Mini logic to correctly format the timestamp here
                 if (answer_upvote_score == 1) {
@@ -442,34 +441,27 @@ IAMA_Extension.UIAnsweredQuestions = function () {
                     question_upvote_score = question_upvote_score.toString() + " upvotes"
                 }
 
-                // Necessary for different messaging color
-                if (key % 2 === 0 || key === 0) {
-                    li_top = $("<li class='left clearfix chat-message-0' id='" + answer_id + "'" + "> </li>");
-
-                } else {
-                    li_top = $("<li class='left clearfix chat-message-1' id='" + answer_id + "'" + "> </li>");
-                }
 
                 // Defining DOM Elements here
                 var separator_q_n_a = $("<hr class='ruler-answered-questions'>"),
 
+                    li_top = $("<li class='left clearfix' id='" + answer_id + "'" + "> </li>"),
+
                     // Building question containers here
-                    q_uber_container = $("<div class='left clearfix'></div>"),
+                    q_uber_container = $("<div class='left clearfix chat-message-question'></div>"),
                     q_header = $("<div class='header'></div>"),
 
                     // Question elements are defined here
                     q_p_username = $("<p class='pull-left primary-font strong'>" + question_author + "</p>"),
                     q_p_posting_time = $("<p class='pull-right text-muted small chat-answered-questions'>" + question_timestamp + "<i class='fa fa-clock-o fa-fw'></i> </p> <br>"),
                     q_p_score = $("<p class='pull-right text-muted small chat-answered-questions'>" + question_upvote_score + "<i class='fa fa-bullseye fa-fw'> </i> </p> <br>"),
-                    // q_p_question_id = $("<p class='pull-right text-muted small chat-answered-questions'>" + question_id + "<i class='fa fa-bookmark fa-fw'> </i> </p> <br> <br>"),
                     q_p_text = $("<p class='chat-alignment-left'>" + question_text + "</p>"),
 
-                    a_uber_container = $("<div class='right clearfix'></div>"),
+                    a_uber_container = $("<div class='right clearfix chat-message-answer'></div>"),
                     a_header = $("<div class='header'></div>"),
                     a_p_username = $("<p class='pull-right primary-font strong'>" + "You" + "</p><br>"),
                     a_p_posting_time = $("<p class='pull-left text-muted small chat-answered-questions'><i class='fa fa-clock-o fa-fw'></i>" + answer_timestamp + "</p><br>"),
                     a_p_score = $("<p class='pull-left text-muted small chat-answered-questions'><i class='fa fa-bullseye fa-fw'></i>" + answer_upvote_score + "</p><br>"),
-                    // a_p_answer_id = $("<p class='pull-left text-muted small chat-answered-questions'><i class='fa fa-bookmark fa-fw'></i>" + answer_id + "</p><br><br>"),
                     a_p_text = $("<p class='chat-alignment-right'>" + answer_text + "</p>");
 
                 // Chains the question objects all together
@@ -477,7 +469,6 @@ IAMA_Extension.UIAnsweredQuestions = function () {
                 q_p_username.appendTo(q_header);
                 q_p_posting_time.appendTo(q_header);
                 q_p_score.appendTo(q_header);
-                // q_p_question_id.appendTo(q_header);
                 q_p_text.appendTo(q_header);
 
                 // Chains the answer objects all together
@@ -485,16 +476,15 @@ IAMA_Extension.UIAnsweredQuestions = function () {
                 a_p_username.appendTo(a_header);
                 a_p_posting_time.appendTo(a_header);
                 a_p_score.appendTo(a_header);
-                // a_p_answer_id.appendTo(a_header);
                 a_p_text.appendTo(a_header);
 
                 // Appends all container objects to the top level DOM <li> element
                 q_uber_container.appendTo(li_top);
-                separator_q_n_a.appendTo(li_top);
                 a_uber_container.appendTo(li_top);
 
                 // Adds that combination to the answer panel
                 answer_Panel.append(li_top);
+                separator_q_n_a.appendTo(answer_Panel);
             });
         },
 
